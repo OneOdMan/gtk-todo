5ee9cb7b98b280af17323f658f3fbaa2b89e587f
OneOdMan <namdo2000@gmail.com>
2024-09-06 16:52:30 -0700

    -- [Main Application Window] -- --[Timestamp]: 06 Sep 2024 | 1653
     Changes committed:
    	modified:   application/ui/todo/application.rb
    			- Accomidating all changes below to main app
    file
    	new file:   application/ui/todo/application_window.rb
    			- Main Application Window class using resource
    			  file created by Glade
    	new file:   resources/ui/application_window.ui
    	new file:   resources/ui/application_window.ui~
    			- Resorce files for main applicaion window
    			  created in Glade

--- application/ui/todo/application.rb
+++ application/ui/todo/application.rb
@@ -4,8 +4,7 @@ module ToDo
       super 'com.oneodman.gtk-todo', Gio::ApplicationFlags::FLAGS_NONE
 
       signal_connect :activate do |application|
-        window = Gtk::ApplicationWindow.new(application)
-        window.set_title 'GTK-ToDo'
+        window = Todo::ApplicationWindow.new(application)
         window.present
       end
     end
